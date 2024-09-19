cd318e4003483805e31ba99fcb398e06f9b9733d
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-19 04:38:52 +0000

    popup.c update

--- popup.c
+++ popup.c
@@ -4,21 +4,25 @@
 static void activate (GtkApplication* app, void *_data) {
     (void)_data;
 
-    // Create a normal GTK window however you like
     GtkWindow *gtk_window = GTK_WINDOW (gtk_application_window_new (app));
 
-    // Before the window is first realized, set it up to be a layer surface
     gtk_layer_init_for_window (gtk_window);
 
-    // Order below normal windows
+    // 레이어 설정
     gtk_layer_set_layer (gtk_window, GTK_LAYER_SHELL_LAYER_TOP);
 
-    // Push other windows out of the way
-    gtk_layer_auto_exclusive_zone_enable (gtk_window);
-    // Set up a widget
+    // 자동 독점 영역 비활성화
+    // gtk_layer_auto_exclusive_zone_enable (gtk_window); // 주석 처리 또는 제거
+
+    // 윈도우 크기를 정사각형으로 설정
+    gtk_window_set_default_size(gtk_window, 300, 300); // 예: 300x300
+
+    // 윈도우를 화면 중앙에 배치
+    gtk_window_set_gravity(gtk_window, GDK_GRAVITY_CENTER);
+
     GtkWidget *label = gtk_label_new ("");
     gtk_label_set_markup (GTK_LABEL (label),
-                          "<span font_desc=\"100.0\">"
+                          "<span font_desc=\"20.0\">"
                               "GTK Layer\nShell example!"
                           "</span>");
     gtk_window_set_child (gtk_window, label);
