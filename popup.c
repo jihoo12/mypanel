e979a3abd28093e04cc5a3ee888726564c4da1df
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-19 04:25:34 +0000

    popup.c formatting code

--- popup.c
+++ popup.c
@@ -1,9 +1,7 @@
 #include "gtk4-layer-shell.h"
 #include <gtk/gtk.h>
 
-static void
-activate (GtkApplication* app, void *_data)
-{
+static void activate (GtkApplication* app, void *_data) {
     (void)_data;
 
     // Create a normal GTK window however you like
@@ -45,9 +43,7 @@ activate (GtkApplication* app, void *_data)
     gtk_window_present (gtk_window);
 }
 
-int
-main (int argc, char **argv)
-{
+int main (int argc, char **argv) {
     GtkApplication * app = gtk_application_new ("com.github.wmww.gtk4-layer-shell.example", G_APPLICATION_FLAGS_NONE);
     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
     int status = g_application_run (G_APPLICATION (app), argc, argv);
